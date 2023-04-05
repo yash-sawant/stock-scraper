2c782a65649999c877b1aa0f793eac61a770efd7
yash-sawant <yash.sawant96@gmail.com>
2023-04-05 12:38:21 -0400

    enabled debugging

--- app.py
+++ app.py
@@ -74,4 +74,4 @@ def evaluation():
 if __name__ == '__main__':
     # port = int(os.environ.get('PORT', 8000))
     # app.run(threaded=False, host='0.0.0.0', port=port)
-    app.run(threaded=False, host='0.0.0.0', port=8000)
+    app.run(debug=True, threaded=False, host='0.0.0.0', port=8000)
